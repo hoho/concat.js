9d3d9350da836572b4cb4e80a27c0ce60e6a6d6d
Marat Abdullin <dakota@brokenpipe.ru>
2013-07-11 14:45:25 +0400

    Better tests.

--- test/test.js
+++ test/test.js
@@ -1,14 +1,172 @@
-test('concat.js parts', function() {
-    var container = document.getElementById('container');
+function domToArray(node) {
+    var ret = [], i, j, n, attr, a;
+
+    for (i = 0; i < node.childNodes.length; i++) {
+        n = node.childNodes[i];
+
+        if (n.nodeType === 3) {
+            ret.push(n.textContent);
+        } else {
+            attr = {};
+            for (j = 0; j < n.attributes.length; j++) {
+                a = n.attributes[j];
+
+                if (a.name === 'style') {
+                    attr[a.name] = n.style.cssText.replace(/'|"|;$/g, '');
+                } else {
+                    attr[a.name] = a.value;
+                }
+            }
+
+            ret.push({name: n.tagName.toLowerCase(), attr: attr, children: domToArray(n)});
+        }
+    }
+
+    return ret;
+}
+
+function attrEqual(val, expected) {
+    var i = 0, j = 0, name;
+
+    for (name in val) {
+        i++;
+    }
+
+    for (name in expected) {
+        j++;
+    }
+
+    equal(i, j, 'Same attribute count');
+
+    for (name in val) {
+        equal(val[name], expected[name], 'Same attribute value');
+    }
+}
+
+function domEqual(val, expected) {
+    var i, attr;
+
+    equal(val.length, expected.length, 'Same node count');
+
+    for (i = 0; i < Math.min(val.length, expected.length); i++) {
+        equal(typeof val[i], typeof expected[i], 'Same node type');
+
+        if (typeof val[i] === 'object') {
+            equal(val[i].name, expected[i].name, 'Same name');
+            attrEqual(val[i].attr, expected[i].attr);
+            domEqual(val[i].children, expected[i].children);
+        } else {
+            equal(val[i], expected[i], 'Same text value');
+        }
+    }
+}
+
+test('concat.js callback context', function() {
+    var container = document.getElementById('container'),
+        dos = [];
+
+    var callback = function(index, item) { return "'" + index + ' ' + item + ' ' + this.tagName + "'"; },
+        docallback = function(index, item) { dos.push(index + ' ' + item + ' ' + this.tagName) };
 
     $C(container)
         .elem('section')
             .text('hello')
+            .do(docallback)
             .text(function() { if (this.tagName === 'SECTION') { return ' world'; }})
+        .end()
+        .repeat(2)
+            .do(docallback)
+            .span()
+                .each(['a', 'b'])
+                    .repeat(3)
+                        .b({test: callback, style: function() { return {content: callback}}})
+                            .text(callback)
+                            .do(docallback)
+                        .end()
+                        .do(docallback)
+                    .end()
+                    .do(docallback)
+                    .u({style: {content: callback}})
+                        .text(callback)
+                        .do(docallback)
+                    .end()
+                .end()
+                .do(docallback)
+                .i()
+                    .do(docallback)
+                    .text(callback)
+                .end()
+            .end()
         .end()
     .end();
 
-    deepEqual(container.innerHTML, '<section>hello world</section>');
+    domEqual(domToArray(container), [
+        {name: 'section', children: ['hello', ' world']},
+        {name: 'span', children: [
+            {name: 'b', attr: {test: "'0 undefined B'", style: "content: 0 undefined B"}, children: ["'0 undefined B'"]},
+            {name: 'b', attr: {test: "'1 undefined B'", style: "content: 1 undefined B"}, children: ["'1 undefined B'"]},
+            {name: 'b', attr: {test: "'2 undefined B'", style: "content: 2 undefined B"}, children: ["'2 undefined B'"]},
+            {name: 'u', attr: {style: "content: 0 a U"}, children: ["'0 a U'"]},
+            {name: 'b', attr: {test: "'0 undefined B'", style: "content: 0 undefined B"}, children: ["'0 undefined B'"]},
+            {name: 'b', attr: {test: "'1 undefined B'", style: "content: 1 undefined B"}, children: ["'1 undefined B'"]},
+            {name: 'b', attr: {test: "'2 undefined B'", style: "content: 2 undefined B"}, children: ["'2 undefined B'"]},
+            {name: 'u', attr: {style: "content: 1 b U"}, children: ["'1 b U'"]},
+            {name: 'i', children: ["'0 undefined I'"]}
+        ]},
+        {name: 'span', children: [
+            {name: 'b', attr: {test: "'0 undefined B'", style: "content: 0 undefined B"}, children: ["'0 undefined B'"]},
+            {name: 'b', attr: {test: "'1 undefined B'", style: "content: 1 undefined B"}, children: ["'1 undefined B'"]},
+            {name: 'b', attr: {test: "'2 undefined B'", style: "content: 2 undefined B"}, children: ["'2 undefined B'"]},
+            {name: 'u', attr: {style: "content: 0 a U"}, children: ["'0 a U'"]},
+            {name: 'b', attr: {test: "'0 undefined B'", style: "content: 0 undefined B"}, children: ["'0 undefined B'"]},
+            {name: 'b', attr: {test: "'1 undefined B'", style: "content: 1 undefined B"}, children: ["'1 undefined B'"]},
+            {name: 'b', attr: {test: "'2 undefined B'", style: "content: 2 undefined B"}, children: ["'2 undefined B'"]},
+            {name: 'u', attr: {style: "content: 1 b U"}, children: ["'1 b U'"]},
+            {name: 'i', children: ["'1 undefined I'"]}
+        ]}
+    ]);
+
+    deepEqual(dos, [
+        'undefined undefined SECTION',
+        '0 undefined undefined',
+        '0 undefined B',
+        '0 undefined SPAN',
+        '1 undefined B',
+        '1 undefined SPAN',
+        '2 undefined B',
+        '2 undefined SPAN',
+        '0 a SPAN',
+        '0 a U',
+        '0 undefined B',
+        '0 undefined SPAN',
+        '1 undefined B',
+        '1 undefined SPAN',
+        '2 undefined B',
+        '2 undefined SPAN',
+        '1 b SPAN',
+        '1 b U',
+        '0 undefined SPAN',
+        '0 undefined I',
+        '1 undefined undefined',
+        '0 undefined B',
+        '0 undefined SPAN',
+        '1 undefined B',
+        '1 undefined SPAN',
+        '2 undefined B',
+        '2 undefined SPAN',
+        '0 a SPAN',
+        '0 a U',
+        '0 undefined B',
+        '0 undefined SPAN',
+        '1 undefined B',
+        '1 undefined SPAN',
+        '2 undefined B',
+        '2 undefined SPAN',
+        '1 b SPAN',
+        '1 b U',
+        '1 undefined SPAN',
+        '1 undefined I'
+    ]);
 
     container.innerHTML = '';
 });
@@ -17,10 +175,8 @@ test('concat.js complex test', function() {
     var container = document.getElementById('container'),
         tmp = 0;
 
-
     $C(container)
-        .div({something: function() { return this.tagName; }})
-            .do(function() { this.addEventListener('click', function() { alert(5555); }); })
+        .div({'class': 'ololo', tmp: undefined, tmp2: function() {}, something: function() { return this.tagName; }})
             .ul()
                 .repeat(2)
                     .li()
@@ -54,8 +210,42 @@ test('concat.js complex test', function() {
         .end(3)
     .end();
 
-    // TODO: Make a cross-browser function to compare resulting HTML.
-    deepEqual(container.innerHTML, '<div something="DIV"><ul><li>aaa</li><li>aaa</li></ul></div><span style="border: 1px solid green;">0</span><span style="border: 1px solid green;">1</span><span style="border: 1px solid green;">2</span><p>0 1</p><p>1 2</p><p>2 3</p><p>3 4</p><p>4 5</p><div>hello<br>world</div><p style="background-color: purple; height: 100px;">0 9<div>0</div><div>1</div><div>0 9</div></p><p style="background-color: purple; height: 100px;">1 8<div>0</div><div>1</div><div>1 8</div></p><p style="background-color: purple; height: 100px;">2 7<div>0</div><div>1</div><div>2 7</div></p>');
+    domEqual(domToArray(container), [
+        {name: 'div', attr: {'class': 'ololo', something: 'DIV'}, children: [
+            {name: 'ul', children: [
+                {name: 'li', children: ['aaa']},
+                {name: 'li', children: ['aaa']}
+            ]}
+        ]},
+        {name: 'span', attr: {style: 'border: 1px solid green'}, children: ['0']},
+        {name: 'span', attr: {style: 'border: 1px solid green'}, children: ['1']},
+        {name: 'span', attr: {style: 'border: 1px solid green'}, children: ['2']},
+        {name: 'p', children: ['0 1']},
+        {name: 'p', children: ['1 2']},
+        {name: 'p', children: ['2 3']},
+        {name: 'p', children: ['3 4']},
+        {name: 'p', children: ['4 5']},
+        {name: 'div', children: ['hello', {name: 'br', children: []}, 'world']},
+        {name: 'p', attr: {style: 'background-color: purple; height: 100px'}, children: [
+            '0 9',
+            {name: 'div', children: ['0']},
+            {name: 'div', children: ['1']},
+            {name: 'div', children: ['0 9']}
+        ]},
+        {name: 'p', attr: {style: 'background-color: purple; height: 100px'}, children: [
+            '1 8',
+            {name: 'div', children: ['0']},
+            {name: 'div', children: ['1']},
+            {name: 'div', children: ['1 8']}
+        ]},
+        {name: 'p', attr: {style: 'background-color: purple; height: 100px'}, children: [
+            '2 7',
+            {name: 'div', children: ['0']},
+            {name: 'div', children: ['1']},
+            {name: 'div', children: ['2 7']}
+        ]}
+    ]);
 
     container.innerHTML = '';
 });
+
